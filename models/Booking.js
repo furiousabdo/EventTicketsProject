3240d8cc5b9c2a4e6b8e46a12628abdd60df3976
Abdulrahman Farajallah <afarajallah36@gmail.com>
2025-02-26 13:49:57 +0200

    Updated Booking

--- models/Booking.js
+++ models/Booking.js
@@ -5,7 +5,7 @@ const bookingSchema = new Schema({
     event: { type: Schema.Types.ObjectId, ref: "Event", required: true },
     numberOfTickets: { type: Number, required: true },
     totalPrice: { type: Number, required: true },
-    status: { type: String, enum: ["Pending", "confirmed", "canceled"], default: "Pending" },
+    status: { type: String, enum: ["Pending", "Confirmed", "canceled"], default: "Pending" },
 }, { timestamps: true });
 
 export default model("Booking", bookingSchema);
