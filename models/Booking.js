2b2fb8e79de83a090791baec2c516795c8955321
Abdulrahman Farajallah <afarajallah36@gmail.com>
2025-04-13 20:21:14 +0200

    created folders to organize classes

--- models/Booking.js
+++ models/Booking.js
@@ -1,16 +1,6 @@
 import { Schema, model } from "mongoose";
 
 const bookingSchema = new Schema({
-<<<<<<< HEAD
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    event: { type: Schema.Types.ObjectId, ref: "Event", required: true },
-    numberOfTickets: { type: Number, required: true },
-    totalPrice: { type: Number, required: true },
-    status: { type: String, enum: ["Pending", "Confirmed", "canceled"], default: "Pending" },
-}, { timestamps: true });
-
-export default model("Booking", bookingSchema)
-=======
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   event: { type: Schema.Types.ObjectId, ref: "Event", required: true },
   ticketsBooked: { type: Number, required: true },
@@ -19,4 +9,3 @@ export default model("Booking", bookingSchema)
 }, { timestamps: true });
 
 export default model("Booking", bookingSchema);
->>>>>>> origin/member-ali
