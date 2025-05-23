b0d9d8f719171a55bad35c86c39859cc1cf9758c
Abdulrahman Farajallah <afarajallah36@gmail.com>
2025-05-23 22:33:54 +0300

    Added reportWebVitals.js, setupTests.js, and theme.js. Also merged my app.js with member-ali

---
+++ client/src/setupTests.js
@@ -0,0 +1,3 @@
+// This file is a custom setup file for Jest and React Testing Library.
+// It is used to configure the testing environment before running tests.
+import '@testing-library/jest-dom';
